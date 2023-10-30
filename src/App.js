e21b33dbf5811212d6bc33943b7239f22ebe7c45
busi-reddy-karnati <bk2444@nyu.edu>
2023-10-30 19:52:58 -0400

    start

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi. I am Busi Reddy Karnati.
+          I am a Software Engineer
         </p>
       </header>
     </div>
